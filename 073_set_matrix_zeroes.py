d36e129f23f9a8477e9e357e0a4767e3f97d6885
D.K.L. Breitling <72890389+dklbreitling@users.noreply.github.com>
2024-05-19 18:46:21 +0200

    add 073_set_matrix_zeroes.py

---
+++ 073_set_matrix_zeroes.py
@@ -0,0 +1,20 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n_rows = len(matrix)
+        n_cols = len(matrix[0])
+        zero_rows = set()
+        zero_cols = set()
+        for r in range(n_rows):
+            for c in range(n_cols):
+                if matrix[r][c] == 0:
+                    zero_rows.add(r)
+                    zero_cols.add(c)
+        for r in zero_rows:
+            for c in range(n_cols):
+                matrix[r][c] = 0
+        for r in range(n_rows):
+            for c in zero_cols:
+                matrix[r][c] = 0
